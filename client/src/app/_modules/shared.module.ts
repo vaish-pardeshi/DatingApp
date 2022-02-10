f1d87afb1458a66c8cb8eef4db1718e6e919fa6b
Vaishnavi Pardeshi <pardeshivaishnavi2018@gmail.com>
2022-02-10 16:33:44 +0530

    End of section 11

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -4,24 +4,27 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
 import { TabsModule } from 'ngx-bootstrap/tabs'
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
-
+import { FileUploadModule } from 'ng2-file-upload';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    FileUploadModule,
     BsDropdownModule.forRoot(),
     ToastrModule.forRoot({
       positionClass:'toast-bottom-right'
     }),
     TabsModule.forRoot(),
-    NgxGalleryModule
+    NgxGalleryModule,
+  
   ],
   exports:[
     BsDropdownModule,
     ToastrModule,
     TabsModule,
-    NgxGalleryModule
+    NgxGalleryModule,
+    FileUploadModule
   ]
 })
 export class SharedModule { }
